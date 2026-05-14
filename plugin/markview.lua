75a07cfa3a1a1cc37196a66051df3c4024141d2b
ggjorven <jorbenvanderwal@gmail.com>
2026-05-14 19:00:42 +0200

    Add markdown preview plugin.

---
+++ plugin/markview.lua
@@ -0,0 +1,15 @@
+vim.pack.add({
+	{
+		src = "https://github.com/OXY2DEV/markview.nvim",
+		name = "markview"
+	}
+})
+
+----------------------------------------------
+-- Setup
+----------------------------------------------
+require("markview").setup({
+	preview = {
+		enable = true
+	}
+});
