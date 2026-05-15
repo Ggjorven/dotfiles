b39cb4787665c902b36763981a58b1078c06ad2f
ggjorven <jorbenvanderwal@gmail.com>
2026-05-15 15:38:25 +0200

    Fix spacing.

--- plugin/spectre.lua
+++ plugin/spectre.lua
@@ -1,15 +1,15 @@
 vim.pack.add({
 	{
-		src = "https://github.com/nvim-lua/plenary.nvim", 
+		src = "https://github.com/nvim-lua/plenary.nvim",
 		name = "plenary",
 	},
 	{
-		src = "https://github.com/nvim-pack/nvim-spectre", 
+		src = "https://github.com/nvim-pack/nvim-spectre",
 		name = "spectre",
 		requires = {
 			"nvim-lua/plenary.nvim",
 		},
-	} 
+	}
 })
 
 ----------------------------------------------
