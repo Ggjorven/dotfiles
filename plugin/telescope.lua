ec51db21e46907862e4b7df276c73ca738cb34ae
ggjorven <jorbenvanderwal@gmail.com>
2026-05-16 16:17:54 +0200

    Fix diagnostics.

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -46,5 +46,5 @@ vim.keymap.set("n", "<leader>fs", function()
     telescope.treesitter()
   end
 end, { desc = "Telescope symbols" })
-vim.keymap.set("n", "<leader>fd", telescope.diagnostics, { desc = "Telescope diagnostics" })
+vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", { desc = "Telescope diagnostics" })
 vim.keymap.set("n", "<leader>fn", "<cmd>Telescope notify<cr>", { desc = "Telescope notifications" })
