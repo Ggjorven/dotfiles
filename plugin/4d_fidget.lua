db2da376a9187dd34d7dae45c689c4d4e1a6c87b
ggjorven <jorbenvanderwal@gmail.com>
2026-05-19 15:54:01 +0200

    Add fidget plugin

---
+++ plugin/4d_fidget.lua
@@ -0,0 +1,13 @@
+vim.pack.add({
+	{
+		src = "https://github.com/j-hui/fidget.nvim.git",
+		name = "fidget"
+	}
+})
+
+----------------------------------------------
+-- Setup
+----------------------------------------------
+require("fidget").setup({
+
+})
